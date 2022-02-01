937956393c28ab974eb633648d398d660afc159d
Taher Ahmed Ashraf <taheirahmedashraf@gmail.com>
2022-02-01 21:13:25 +0600

    Token Verification Middleware added

---
+++ app/middlewares/token.verification.js
@@ -0,0 +1,18 @@
+const fs = require("fs");
+const PRIVATE_KEY = fs.readFileSync("./private.key", "utf8");
+
+const TokenVerification = async (req, res, next) => {
+  try {
+    const decodedToken = jwt.verify(req.body.access_token, PRIVATE_KEY);
+    if (decodedToken) {
+      req.decodedToken = decodedToken;
+      next();
+    } else {
+      res.status(403).json({ message: "Forbidden", status: 403 });
+    }
+  } catch (error) {
+    res.status(403).json({ message: "Forbidden", status: 403 });
+  }
+};
+
+module.exports = TokenVerification;
